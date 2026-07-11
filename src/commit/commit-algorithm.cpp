e268a4d9940f3116333416663b6bc41405fee2cf
DozenOS autobuild <autobuild@dozenos.local>
2026-07-11 12:35:21 +0000

    sync: rename-transform snapshot (upstream @7f8beb2)
    DozenOS mirror -- idempotent rename-transform applied (vyatta preserved), upstream .github/ stripped. Upstream commit: 7f8beb2

--- src/commit/commit-algorithm.cpp
+++ src/commit/commit-algorithm.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2011 Vyatta, Inc.
+ * Modifications Copyright DozenOS Contributors. See git history for details.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
